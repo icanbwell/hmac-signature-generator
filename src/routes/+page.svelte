dbbe3c95de628482628e9ebeeadb270ace17d6d0
Kyle Wade <kyle.wade@icanbwell.com>
2024-09-26 12:07:47 -0400

    Get tailwind working

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>HMAC Generator</h1>
+<p>Enter the values below to generate a valid signature and cURL.</p>
+
+<div class="grid grid-cols-1 gap-6 md:grid-cols-2">
+  <div class="flex flex-col">
+    <label for="x-bwell-date" class="mb-2">x-bwell-date</label>
+    <input
+      type="datetime-local"
+      placeholder="Enter date..."
+      id="x-bwell-date"
+      class="p-2.5 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+    />
+  </div>
+
+  <div class="flex flex-col">
+    <label for="x-bwell-client-key" class="mb-2">x-bwell-client-key</label>
+    <textarea
+      placeholder="Enter client key..."
+      id="x-bwell-client-key"
+      class="p-2.5 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+    ></textarea>
+  </div>
+</div>
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import tailwindcss from 'tailwindcss';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	css: {
+		postcss: {
+			plugins: [tailwindcss()],
+		},
+	},
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
