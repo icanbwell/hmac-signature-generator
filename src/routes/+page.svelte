128faa3b033693b566d1f2bbd08d7fd2980e274d
Kyle Wade <kyle.wade@icanbwell.com>
2024-09-26 15:45:25 -0400

    Add hmacSecret variable

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -23,6 +23,7 @@
     "z4PhNX7vuL3xVChQ1m2AB9Yg5AULVxXcg/SpIdNs6c5H0NE8XYXysP+DGNKHfuwvY7kxvUdBeoGlODJ6+SfaPg==";
   let xBwellClientKey = "";
   let xBwellClientUserToken = "";
+  let hmacSecret = "";
   let hmacSignature = "";
   let host = "";
   let path = "";
