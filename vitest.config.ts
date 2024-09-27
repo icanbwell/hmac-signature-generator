875b3b4ddc71e4c96fe715330082929bf4c78025
Kyle Wade <kyle.wade@icanbwell.com>
2024-09-27 10:04:31 -0400

    Fix vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,5 +4,6 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
     test: {
         environment: 'happy-dom', // Use a browser-like environment in tests
+        include: ['src/**/*.{test,spec}.{js,ts}'], // Only run tests in the 'src' directory
     },
 });
